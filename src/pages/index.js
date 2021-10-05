9b20f0f5232155ac6cd4e66b8c98e980dfb0fb6e
Song kyeong seok <skj0922@kookmin.ac.kr>
2021-10-05 15:53:29 +0900

    feat: page 추가 및 index.js export 추가

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,1 +1,5 @@
 export { default as Home } from 'pages/home/home';
+export { default as Profile } from 'pages/profile/profile';
+export { default as ReviewWrite } from 'pages/reviewWrite/reviewWrite';
+export { default as Review } from 'pages/review/review';
+export { default as list } from 'pages/list/list';
---
+++ src/pages/list/list.jsx
@@ -0,0 +1,9 @@
+import ReviewRank from 'components/ReviewRank';
+
+export default function Home() {
+  return (
+    <>
+      <ReviewRank />
+    </>
+  );
+}
