8fcf5ca8a7be2a9f479901ded91250c4d4dd8726
jseo9732 <jseo9732@gmail.com>
2021-10-07 18:31:16 +0900

    Design: css-in-js로 수정

--- src/components/ReviewCard.js
+++ src/components/ReviewCard.js
@@ -1,38 +1,132 @@
-import './ReviewCard.css';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  display: inline-block;
+  width: 260px;
+  height: 288px;
+  background: #FFFFFF 0% 0% no-repeat padding-box;
+  border-radius: 10px;
+  margin-right: 20px;
+`;
+
+const Img = styled.img.attrs(props => ({
+  src : props.src,
+  alt : props.alt,
+  title : props.title
+}))`
+  display: block;
+  width: 130px;
+  height: 130px;
+  border: 1px solid #DEDEDE;
+  border-radius: 50%;
+  margin: 0 auto;
+  margin-bottom: 10px;
+`;
+
+const Info = styled.div`
+  margin: 5px;
+`;
+
+const Group = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  align-items: flex-end;
+`;
+
+const Category = styled.div`
+  background: #F2F2F2 0% 0% no-repeat padding-box;
+  border-radius: 5px;
+  opacity: 1;
+  text-align: left;
+  letter-spacing: 0px;
+  color: #808084;
+  opacity: 1;
+  font-size: 11px;
+  padding: 6px 8px;
+  border-radius: 5px;
+  margin-bottom: 5px;
+`;
+
+const ChannelName = styled.div`
+  width: 130px;
+  overflow: hidden;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+  font-size: 18px;
+  font-weight: bold;
+  display: inline-block;
+  padding: 5px 5px 5px 0;
+`;
+
+const SubscriberCount = styled.div`
+  font-size: 14px;
+  color: #94969b;
+  padding-bottom: 5px;
+`;
+
+const Ratings = styled.div`
+  font-size: 14px;
+  font-weight: bold;
+  padding: 0px 5px 15px 0;
+`;
+
+const ReviewCount = styled.div`
+  font-size: 14px;
+  color: #94969b;
+  padding-bottom: 15px;
+`;
+
+const BestReviewTitle = styled.div`
+  font-size: 12px;
+  font-weight: bold;
+  padding-bottom: 5px;
+`;
+
+const BestReview = styled.div`
+  width: 250px;
+  font-size: 12px;
+  font-weight: lighter;
+  color: #808084;
+
+  overflow: hidden;
+  text-overflow: ellipsis;
+  display: -webkit-box;
+  -webkit-line-clamp: 2;
+  -webkit-box-orient: vertical;
+`;
 
 export default function ReviewCard({
   category,
   channelName,
   subscriberCount,
   reviewCount,
-  rating,
+  ratings,
   bestReview,
 }) {
   return (
-    <div className="review-card-container">
-      <img
-        className="yt_img"
+    <Container>
+      <Img 
         src={require('./월간윤종신.jpg').default}
         alt={channelName}
         title={channelName}
       />
-      <div className="review-card-info">
-        <div className="info-group">
-          <div className="category">{category}</div>
-        </div>
-        <div className="info-group">
-          <div className="channelName">{channelName}</div>
-          <div className="subscriberCount">구독자 {subscriberCount} 명</div>
-        </div>
-        <div className="info-group">
-          <div className="rating">❤❤❤❤❤ {rating}</div>
-          <div className="review-count">({reviewCount}개 리뷰)</div>
-        </div>
-        <div className="info-group">
-          <div className="best-review-title">Best Review</div>
-          <div className="best-review">{bestReview}</div>
-        </div>
-      </div>
-    </div>
+      <Info>
+        <Group>
+          <Category>{category}</Category>
+        </Group>
+        <Group>
+          <ChannelName>{channelName}</ChannelName>
+          <SubscriberCount>구독자 {subscriberCount} 명</SubscriberCount>
+        </Group>
+        <Group>
+          <Ratings>★★★★★ {ratings}</Ratings>
+          <ReviewCount>({reviewCount}개 리뷰)</ReviewCount>
+        </Group>
+        <Group>
+          <BestReviewTitle>Best Review</BestReviewTitle>
+          <BestReview>{bestReview}</BestReview>
+        </Group>
+      </Info>
+    </Container>
   );
 }
--- src/components/ReviewRank.js
+++ src/components/ReviewRank.js
@@ -1,18 +1,42 @@
 import ReviewCard from './ReviewCard';
-import './ReviewRank.css';
+import styled from 'styled-components';
+
+const RowContainer = styled.div`
+  white-space: nowrap;
+  overflow-x: scroll;
+  // overflow: hidden;
+`;
+
+const RowTitle = styled.h2`
+  font-size: 20px;
+  font-weight: bold;
+  margin: 0px;
+  margin-bottom: 5px;
+`;
+
+const RowDescription = styled.div`
+  font-size: 14px;
+  color: #94969B;
+  font-weight: lighter;
+  margin-bottom: 20px;
+`;
+
+const CardContainer = styled.div`
+  margin-bottom: 60px;
+`;
 
 export default function ReviewRank() {
   return (
-    <div className="row-container">
-      <h2 className="row-title">이번주 리뷰가 많은 유튜버</h2>
-      <div className="row-title-description">리뷰 보러 가보실까요?</div>
-      <div className="row-card-container">
+    <RowContainer>
+      <RowTitle>이번주 리뷰가 많은 유튜버</RowTitle>
+      <RowDescription>리뷰 보러 가보실까요?</RowDescription>
+      <CardContainer>
         <ReviewCard
           category="음악"
           channelName="월간 윤종신"
           subscriberCount="22.5만"
           reviewCount="381"
-          rating="5.0"
+          ratings="5.0"
           bestReview="제가 많이 힘들고 혼란스러웠을 때 큰 도움이 되었던 노래였습니다. 그 당시에는 멜로디는 잔잔하지만"
         />
         <ReviewCard
@@ -20,7 +44,7 @@ export default function ReviewRank() {
           channelName="딩고 뮤직 / dingo music"
           subscriberCount="329만"
           reviewCount="500"
-          rating="4.0"
+          ratings="4.0"
           bestReview="제가 많이 힘들고 혼란스러웠을 때 큰 도움이 되었던 노래였습니다. 그 당시에는 멜로디는 잔잔하지만"
         />
         <ReviewCard
@@ -28,7 +52,7 @@ export default function ReviewRank() {
           channelName="월간 윤종신"
           subscriberCount="22.5만"
           reviewCount="381"
-          rating="5.0"
+          ratings="5.0"
           bestReview="제가 많이 힘들고 혼란스러웠을 때 큰 도움이 되었던 노래였습니다. 그 당시에는 멜로디는 잔잔하지만"
         />
         <ReviewCard
@@ -36,7 +60,7 @@ export default function ReviewRank() {
           channelName="월간 윤종신"
           subscriberCount="22.5만"
           reviewCount="381"
-          rating="5.0"
+          ratings="5.0"
           bestReview="제가 많이 힘들고 혼란스러웠을 때 큰 도움이 되었던 노래였습니다. 그 당시에는 멜로디는 잔잔하지만"
         />
         <ReviewCard
@@ -44,7 +68,7 @@ export default function ReviewRank() {
           channelName="월간 윤종신"
           subscriberCount="22.5만"
           reviewCount="381"
-          rating="5.0"
+          ratings="5.0"
           bestReview="제가 많이 힘들고 혼란스러웠을 때 큰 도움이 되었던 노래였습니다. 그 당시에는 멜로디는 잔잔하지만"
         />
         <ReviewCard
@@ -52,7 +76,7 @@ export default function ReviewRank() {
           channelName="월간 윤종신"
           subscriberCount="22.5만"
           reviewCount="381"
-          rating="5.0"
+          ratings="5.0"
           bestReview="제가 많이 힘들고 혼란스러웠을 때 큰 도움이 되었던 노래였습니다. 그 당시에는 멜로디는 잔잔하지만"
         />
         <ReviewCard
@@ -60,10 +84,10 @@ export default function ReviewRank() {
           channelName="월간 윤종신"
           subscriberCount="22.5만"
           reviewCount="381"
-          rating="5.0"
+          ratings="5.0"
           bestReview="제가 많이 힘들고 혼란스러웠을 때 큰 도움이 되었던 노래였습니다. 그 당시에는 멜로디는 잔잔하지만"
         />
-      </div>
-    </div>
+      </CardContainer>
+    </RowContainer>
   );
 }
