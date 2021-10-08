8ddd99cd86e728cde5bb70f2051c33698c73d29b
jseo9732 <jseo9732@gmail.com>
2021-10-08 19:51:17 +0900

    Rename: App.js 경로 수정

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import GlobalStyle from './globalStyle'
-import App from './App';
+import App from './components/App';
 
 ReactDOM.render(
   <React.StrictMode>
