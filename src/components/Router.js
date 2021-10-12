eb69a48e1173e0eb2bb0953a80531df229ed92ce
jseo9732 <jseo9732@gmail.com>
2021-10-12 17:26:57 +0900

    Refactor: Router.js 파일 Switch추가 및 수정

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,14 +1,16 @@
 import React from 'react';
-import { HashRouter as Router, Route } from 'react-router-dom';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import { HeaderContainer, Home, List } from 'pages/index';
 
 export default function AppRouter() {
   return (
     <>
-      <HeaderContainer />
       <Router>
-        <Route exact path="/" component={Home} />
-        <Route path="/list" component={List} />
+        <HeaderContainer />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/list" component={List} />
+        </Switch>
       </Router>
     </>
   );
