c6be6ca9e1843405b11b732f853795f06474121e
Song kyeong seok <skj0922@kookmin.ac.kr>
2021-10-13 01:24:07 +0900

    refactor: 헤더 버튼 컴포넌트 수정

--- src/components/header/headerStyle.js
+++ src/components/header/headerStyle.js
@@ -3,9 +3,9 @@ import { Button, Input } from 'semantic-ui-react';
 
 export const Logo = styled.span`
   font-family: 'SCD-9';
-  font-size: 30px;
+  font-size: 29px;
   padding: 5px;
-  margin-right: 30px;
+  margin-right: 20px;
 `;
 
 export const Positioner = styled.div`
@@ -13,28 +13,46 @@ export const Positioner = styled.div`
   flex-direction: column;
   position: fixed;
   top: 0px;
+  margin: 0px 35px;
   width: 100%;
   z-index: 1;
 `;
 
 export const Background = styled.div`
   background: white;
+  justify-content: left;
   display: flex;
   height: auto;
   padding: 10px 0px;
 `;
 
-export const HeaderButton = styled(Button)`
+export const LoginButton = styled(Button)`
   font-family: 'SHSN-M';
   &&& {
-    font-size: 10px;
+    font-size: 12px;
     margin-left: 20px;
+    border-radius: 0px;
+    width: 80px;
+    height: 38px;
+  }
+`;
+
+export const ReviewButton = styled(LoginButton)`
+  &&& {
+    margin-left: 15px;
+    width: 140px;
+  }
+`;
+export const RegisterButton = styled(LoginButton)`
+  &&& {
+    margin-left: 26px;
+    width: 100px;
   }
 `;
 
 export const Search = styled(Input)`
-  width: 30%;
-  margin-left: 100px;
+  width: 350px;
+  margin-left: 190px;
   &&& > input {
     border-radius: 40px;
   }
