2d9904be3deb9b316cd947143d1d9834369e7234
jseo9732 <jseo9732@gmail.com>
2021-10-13 16:06:11 +0900

    Refactor: Container 위치 수정

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { HeaderContainer, Home, List } from 'pages/index';
+import * as style from './styles/ContainerStyle';
 
 export default function AppRouter() {
   return (
@@ -8,8 +9,10 @@ export default function AppRouter() {
       <Router>
         <HeaderContainer />
         <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/list" component={List} />
+          <style.Container>
+            <Route exact path="/" component={Home} />
+            <Route path="/list" component={List} />
+          </style.Container>
         </Switch>
       </Router>
     </>
