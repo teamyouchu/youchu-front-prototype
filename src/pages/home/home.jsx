45bf5c36a87cb53cac5d2ee94d48651162947d7d
jseo9732 <jseo9732@gmail.com>
2021-10-18 18:13:04 +0900

    Refactor: ReviewRank 컴포넌트 이동

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,10 +1,24 @@
-import ReviewRank from 'components/ReviewRank';
 import Recommend from 'components/Recommend';
+import ReviewCard from 'components/ReviewCard';
 import * as style from './style';
 
 export default function Home() {
+  const slide_L = () =>
+    document.getElementById('slider').scrollBy({
+      top: 0,
+      left: -280,
+      behavior: 'smooth',
+    });
+
+  const slide_R = () =>
+    document.getElementById('slider').scrollBy({
+      top: 0,
+      left: 280,
+      behavior: 'smooth',
+    });
+
   return (
-    <>
+    <style.HomeContainer>
       <style.BannerContainer>
         <style.TextField>
           여러분들만 아는 <br />
@@ -12,8 +26,112 @@ export default function Home() {
         </style.TextField>
         <style.BannerImg src='/images/banner.png' />
       </style.BannerContainer>
-      <ReviewRank />
+      <style.RowContainer>
+        <style.RowTitle>이번주 리뷰가 많은 유튜버 ✌</style.RowTitle>
+        <style.RowDescription>리뷰 보러 가보실까요?</style.RowDescription>
+        <style.HandlePrev onClick={slide_L}>
+          <style.ArrowImg src='/images/back.png' />
+        </style.HandlePrev>
+        <style.ReviewCardContainer id="slider">
+          <ReviewCard
+            img="월간 윤종신"
+            category="음악"
+            channelName="월간 윤종신"
+            subscriberCount="22.5만"
+            reviewCount="381"
+            ratings="5.0"
+            bestReview="제가 많이 힘들고 혼란스러웠을 때 큰 도움이 되었던 노래였습니다. 그 당시에는 멜로디는 잔잔하지만 큰 위로가"
+          />
+          <ReviewCard
+            img="딩고 뮤직"
+            category="엔터테이너"
+            channelName="딩고 뮤직 / dingo music"
+            subscriberCount="329만"
+            reviewCount="500"
+            ratings="3.3"
+            bestReview="제가 많이 힘들고 혼란스러웠을 때 큰 도움이 되었던 노래였습니다. 그 당시에는 멜로디는 잔잔하지만"
+          />
+          <ReviewCard
+            img="고닝 gonying"
+            category="여행"
+            channelName="고닝 gonying"
+            subscriberCount="21"
+            reviewCount="2"
+            ratings="5.0"
+            bestReview="병맛이라 재밌어요 기대가 되는 채널이에요."
+          />
+          <ReviewCard
+            img="월간 윤종신"
+            category="음악"
+            channelName="월간 윤종신"
+            subscriberCount="22.5만"
+            reviewCount="381"
+            ratings="5.0"
+            bestReview="제가 많이 힘들고 혼란스러웠을 때 큰 도움이 되었던 노래였습니다. 그 당시에는 멜로디는 잔잔하지만 큰 위로가"
+          />
+          <ReviewCard
+            img="월간 윤종신"
+            category="음악"
+            channelName="월간 윤종신"
+            subscriberCount="22.5만"
+            reviewCount="381"
+            ratings="5.0"
+            bestReview="제가 많이 힘들고 혼란스러웠을 때 큰 도움이 되었던 노래였습니다. 그 당시에는 멜로디는 잔잔하지만 큰 위로가"
+          />
+          <ReviewCard
+            img="월간 윤종신"
+            category="음악"
+            channelName="월간 윤종신"
+            subscriberCount="22.5만"
+            reviewCount="381"
+            ratings="5.0"
+            bestReview="제가 많이 힘들고 혼란스러웠을 때 큰 도움이 되었던 노래였습니다. 그 당시에는 멜로디는 잔잔하지만 큰 위로가"
+          />
+          <ReviewCard
+            img="월간 윤종신"
+            category="음악"
+            channelName="월간 윤종신"
+            subscriberCount="22.5만"
+            reviewCount="381"
+            ratings="5.0"
+            bestReview="제가 많이 힘들고 혼란스러웠을 때 큰 도움이 되었던 노래였습니다. 그 당시에는 멜로디는 잔잔하지만 큰 위로가"
+          />
+          <ReviewCard
+            img="월간 윤종신"
+            category="음악"
+            channelName="월간 윤종신"
+            subscriberCount="22.5만"
+            reviewCount="381"
+            ratings="5.0"
+            bestReview="제가 많이 힘들고 혼란스러웠을 때 큰 도움이 되었던 노래였습니다. 그 당시에는 멜로디는 잔잔하지만 큰 위로가"
+          />
+          <ReviewCard
+            img="월간 윤종신"
+            category="음악"
+            channelName="월간 윤종신"
+            subscriberCount="22.5만"
+            reviewCount="381"
+            ratings="5.0"
+            bestReview="제가 많이 힘들고 혼란스러웠을 때 큰 도움이 되었던 노래였습니다. 그 당시에는 멜로디는 잔잔하지만 큰 위로가"
+          />
+          <ReviewCard
+            img="월간 윤종신"
+            category="음악"
+            channelName="월간 윤종신"
+            subscriberCount="22.5만"
+            reviewCount="381"
+            ratings="5.0"
+            bestReview="제가 많이 힘들고 혼란스러웠을 때 큰 도움이 되었던 노래였습니다. 그 당시에는 멜로디는 잔잔하지만 큰 위로가"
+          />
+        </style.ReviewCardContainer>
+        <style.HandleNext onClick={slide_R}>
+          <style.ArrowImg src='/images/next.png' />
+        </style.HandleNext>
+      </style.RowContainer>
+
+
+
       <Recommend />
-    </>
+    </style.HomeContainer>
   );
 }
--- src/pages/home/style.js
+++ src/pages/home/style.js
@@ -1,5 +1,10 @@
 import styled from 'styled-components';
 
+export const HomeContainer = styled.div`
+  width: 1100px;
+  margin: 0 auto;
+`;
+
 export const BannerContainer = styled.div`
   display: flex;
   width: 1100px;
@@ -26,4 +31,70 @@ export const BannerImg = styled.img`
   width: 422px;
   height: 276px;
   margin: 49px;
+`;
+
+export const RowContainer = styled.div`
+  width: 1100px;
+  margin: 0 auto;
+`;
+
+export const RowTitle = styled.div`
+  font-family: 'SHSN-B';
+  font-size: 20px;
+  line-height: 25px;
+  margin-bottom: 5px;
+`;
+
+export const RowDescription = styled.div`
+  font-family: 'SHSN-L';
+  font-size: 14px;
+  line-height: 18px;
+  color: #94969b;
+  margin-bottom: 20px;
+`;
+
+export const ArrowImg = styled.img.attrs((props) => ({
+  src: props.src,
+}))`
+  width: 15px;
+  height: 15px;
+`;
+
+export const HandlePrev = styled.button`
+  background: #ffffff;
+  border: 0;
+  cursor: pointer;
+  position: relative;
+  top: 75px;
+  left: -35px;
+  width: 35px;
+`;
+
+export const HandleNext = styled.button`
+  background: #ffffff;
+  border: 0;
+  cursor: pointer;
+  position: relative;
+  top: -302px;
+  left: 1100px;
+  width: 35px;
+`;
+
+export const CardContainer = styled.div`
+  display: grid;
+`;
+
+export const ReviewCardContainer = styled(CardContainer)`
+  white-space: nowrap;
+  overflow-x: scroll;
+  overflow: hidden;
+  grid-template-columns: repeat(10, 1fr);
+  column-gap: 20px;
+  margin-bottom: 60px;
+`;
+
+export const RecommendCardContainer = styled(CardContainer)`
+  grid-template-columns: repeat(3, 1fr);
+  column-gap: 25px;
+  row-gap: 10px;
 `;
