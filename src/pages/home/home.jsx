a60e4821e5ad5c699664c4d8ef591c556880d5d9
jseo9732 <jseo9732@gmail.com>
2021-10-18 18:39:26 +0900

    Design: 배너 이모지 추가

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -22,7 +22,7 @@ export default function Home() {
       <style.BannerContainer>
         <style.TextField>
           여러분들만 아는 <br />
-          <style.RedSpan>유튜버</style.RedSpan>들을 리뷰해봐요
+          <style.RedSpan>유튜버</style.RedSpan>들을 리뷰해봐요 👍
         </style.TextField>
         <style.BannerImg src='/images/banner.png' />
       </style.BannerContainer>
