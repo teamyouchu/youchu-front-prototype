762cd3880fdecf31670a42682e177f1889d6081c
jseo9732 <jseo9732@gmail.com>
2021-10-27 18:19:01 +0900

    Feat: 로그인 페이지 추가

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,4 +3,5 @@ export { default as Profile } from 'pages/profile/profile';
 export { default as ReviewWrite } from 'pages/reviewWrite/reviewWrite';
 export { default as Review } from 'pages/review/review';
 export { default as List } from 'pages/list/list';
+export { default as Login } from 'pages/login/login';
 export { default as TopBar } from 'components/header/TopBar';
