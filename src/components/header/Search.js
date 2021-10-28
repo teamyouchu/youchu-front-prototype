8d23dfb78be9fe5b96df7c8fd7463b7a8c830f43
jseo9732 <jseo9732@gmail.com>
2021-10-28 17:37:36 +0900

    Design: 검색창 돋보기 이미지 교체

--- src/components/header/Search.js
+++ src/components/header/Search.js
@@ -4,7 +4,7 @@ export default function Search() {
   return (
     <>
       <style.SearchContainer>
-        <style.SearchImg src="/images/searchIcon.png" />
+        <style.SearchImg src="/images/searchIcon.svg" />
         <style.InputYoutuber placeholder="유튜버 이름으로 검색하세요"></style.InputYoutuber>
       </style.SearchContainer>
     </>
