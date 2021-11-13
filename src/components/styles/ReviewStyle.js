71646f7a3710d5be6c5e8ccde8b9f0f1636abf5d
Song kyeong seok <skj0922@kookmin.ac.kr>
2021-11-13 12:03:43 +0900

    feat: 자세한 리뷰 컨포넌트 완성

--- src/components/styles/ReviewStyle.js
+++ src/components/styles/ReviewStyle.js
@@ -13,59 +13,50 @@ export const ReviewContainer = styled(FlexContainer)`
   height: 400px;
   width: 100%;
   padding: 20px 0px 20px 40px;
-`;
-
-export const BoldSpan = styled.span`
-  font-family: 'SHSN-B';
-  font-size: 14px;
-`;
-
-export const RegularSpan = styled.span`
-  font-size: 14px;
-  font-family: 'SHSN-R';
-  margin-bottom: 20px;
-`;
-
-export const DateSpan = styled.span`
-  color: #94969b;
-  font-size: 12px;
-  font-family: 'SHSN-M';
-  margin: 5px 0px 20px;
-`;
-
-export const ReviewContent = styled(BoldSpan)`
-  font-size: 18px;
-`;
-
-export const StarBase = styled.div`
-  font-family: 'SHSN-M';
-  font-size: 16px;
-  line-height: 18px;
-  color: #d8d8d8;
-  z-index: 0;
-`;
-
-export const StarRatings = styled.div`
   position: relative;
-  margin-top: 5px;
 `;
 
-export const StarFill = styled.div`
-  font-family: 'SHSN-M';
-  font-size: 16px;
-  line-height: 18px;
-  color: #f8d26a;
-  z-index: 1;
+export const Span = styled.span`
+  font-family: ${(props) => props.font};
+  font-size: ${(props) => props.size};
+  color: ${(props) => props.color};
+  margin: ${(props) => props.margins};
+`;
+
+export const BestSpan = styled(Span)`
   position: absolute;
-  top: 0;
-  left: 0;
-  overflow: hidden;
-  width: ${(props) => props.ratings + '%'};
+  left: 135px;
 `;
 
-export const Ratings = styled.div`
-  font-family: 'SHSN-M';
-  font-size: 14px;
-  line-height: 18px;
-  padding: 0px 5px 15px 0;
+export const UtilContainer = styled(FlexContainer)`
+  align-items: center;
+`;
+
+export const LikeButton = styled.div`
+  width: 67px;
+  height: 32px;
+  border: 1px solid #dedede;
+  border-radius: 5px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 9px 13px;
+  margin-right: 10px;
+  &:hover {
+    cursor: pointer;
+  }
+`;
+
+export const LikeImg = styled.img`
+  width: 14px;
+  height: 13px;
+  margin-right: 11px;
+`;
+
+export const ReportButton = styled.span`
+  font-family: 'SHSN-R';
+  color: #94969b;
+  &:hover {
+    cursor: pointer;
+  }
 `;
