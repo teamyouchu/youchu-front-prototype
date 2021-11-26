c5093995607bf74ee627f3a66c6b0efe537b6582
jseo9732 <jseo9732@gmail.com>
2021-11-26 23:13:17 +0900

    Design: 로그인 페이지 디자인 수정

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -3,15 +3,13 @@ import * as style from './style';
 export default function Login() {
   return (
     <style.LoginContainer>
-      <style.Title>유튜버를 추천받고<br /> 유튜버 리뷰를 남겨보세요!</style.Title>
-      <style.LoginBtn>
-        <style.GoogleLogo />
-        <style.LoginLetter>구글로 로그인</style.LoginLetter>
-      </style.LoginBtn>
-      <style.NewLetter>신규 사용자이신가요?</style.NewLetter>
-      <style.CreateLetter href='https://accounts.google.com/signin/v2/identifier?service=accountsettings&continue=https%3A%2F%2Fmyaccount.google.com%2F%3Fhl%3Dko&hl=ko&ec=GAlAwAE&flowName=GlifWebSignIn&flowEntry=AddSession'>
-        계정 만들기
-      </style.CreateLetter>
+      <style.LoginBox>
+        <style.Title>유튜버를 추천받고<br /> 유튜버 리뷰를 남겨보세요!</style.Title>
+        <style.LoginBtn>
+          <style.GoogleLogo />
+          <style.LoginLetter>구글로 로그인</style.LoginLetter>
+        </style.LoginBtn>
+      </style.LoginBox>
     </style.LoginContainer>
   );
 }
