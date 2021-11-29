8e9a3900c608609d61cf1734be153dffa9f3fe1b
jseo9732 <jseo9732@gmail.com>
2021-11-30 02:43:38 +0900

    Refactor: 개선 헤더 코드

--- src/components/header/TopBar.js
+++ src/components/header/TopBar.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { useHistory } from 'react-router-dom';
-import Search from './Search';
+import { useHistory, Link } from 'react-router-dom';
 import * as style from './headerStyle';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
 
@@ -21,26 +20,23 @@ export default function TopBar() {
     history.push('/list');
   }
 
-  function handleLogoBtnClick() {
-    history.push('/');
-  }
-
-  function handleLoginBtnClick() {
-    history.push('/login');
-  }
-
   return (
     <style.HeaderContainer className={isScrolled ? 'scrolled' : undefined}>
-      <style.Background>
-        <style.Logo onClick={handleLogoBtnClick}>YouChu</style.Logo>
+      <style.HeaderBox>
+        <style.LogoLink to="/">YouChu</style.LogoLink>
         <style.ReviewButton onClick={handleReviewBtnClick} color="black">
           <style.BarIcon icon={faBars} />
           유튜버 리뷰
         </style.ReviewButton>
-        <Search />
+        <style.SearchForm>
+          <style.SearchImg src="/images/searchIcon.svg" />
+          <style.SearchInput placeholder="유튜버 이름으로 검색하세요"></style.SearchInput>
+        </style.SearchForm>
         <style.RegisterButton color="red">유튜버 등록</style.RegisterButton>
-        <style.LoginButton onClick={handleLoginBtnClick}>로그인</style.LoginButton>
-      </style.Background>
+        <Link to="/login">
+          <style.LoginButton>로그인</style.LoginButton>
+        </Link>
+      </style.HeaderBox>
     </style.HeaderContainer>
   );
 }
