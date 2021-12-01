84ba44546f86886fe24ea2046332979889343c75
Song kyeong seok <skj0922@kookmin.ac.kr>
2021-12-01 20:43:05 +0900

    feat: 유튜버 등록 모달창 뷰 구현 완료 (연결 및 기능 X)

---
+++ src/components/styles/RegistrarionStyle.js
@@ -0,0 +1,66 @@
+import styled from 'styled-components';
+
+export const ModalContainer = styled.div`
+  width: 100%;
+  height: 100vh;
+  background-color: rgba(0, 0, 0, 0.4);
+  z-index: 10;
+  position: fixed;
+  top: 0;
+  left: 0;
+`;
+
+export const Modal = styled.div`
+  width: 500px;
+  height: 386px;
+  padding: 70px 60px;
+  background-color: #fff;
+  position: absolute;
+  display: flex;
+  flex-direction: column;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  z-index: 100;
+`;
+
+export const Span = styled.span`
+  font-family: ${(props) => props.font};
+  font-size: ${(props) => props.size};
+  color: ${(props) => props.color};
+  margin: ${(props) => props.margins};
+`;
+
+export const RediretInfo = styled(Span)`
+  cursor: pointer;
+  margin: 5px 0px;
+`;
+
+export const LinkInput = styled.input`
+  border: 1px solid #dedede;
+  width: 380px;
+  height: 40px;
+  border-radius: 5px;
+  padding: 11px 20px;
+  margin-bottom: 30px;
+  font-family: 'SHSN-L';
+  font-size: 14px;
+  line-height: 18px;
+  letter-spacing: 0px;
+
+  &:focus {
+    outline: none;
+  }
+`;
+
+export const CompleteButton = styled.button`
+  width: 380px;
+  height: 55px;
+  background-color: #000000;
+  color: #ffffff;
+  font-family: 'SHSN-M';
+  font-size: 19px;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+`;
