9fb9eda3c44bbd116461e26fed86889718479bb3
jisu Seo <jseo9732@gmail.com>
2022-01-06 16:47:57 +0900

    Design: 추가 선호 카테고리 클릭 이벤트

--- src/pages/signup/signup.jsx
+++ src/pages/signup/signup.jsx
@@ -1,6 +1,18 @@
+import { useState } from 'react';
 import * as style from './style';
 
 export default function Signup() {
+  const [categoryList, setCategoryList] = useState([]);
+
+  const onClick = (e) => {
+    const isIncludes = categoryList.find((el) => el === e.target.value);
+    if (isIncludes) {
+      setCategoryList(categoryList.filter((el) => el !== e.target.value));
+    } else {
+      setCategoryList(preList => [...preList, e.target.value]);
+    }
+  }
+  console.log(categoryList);
   return (
     <style.SignupContainer>
       <style.SignupBox>
@@ -9,24 +21,77 @@ export default function Signup() {
         <style.NicknameInput />
         <style.SubTitle>유튜버 관심 카테고리 선택 (1개 이상)</style.SubTitle>
         <style.CategoryContainer>
-          <style.CategoryBox>영화/애니메이션</style.CategoryBox>
-          <style.CategoryBox>자동차/교통</style.CategoryBox>
-          <style.CategoryBox>음악</style.CategoryBox>
-          <style.CategoryBox>애완동물/동물</style.CategoryBox>
-          <style.CategoryBox>스포츠</style.CategoryBox>
-          <style.CategoryBox>여행/이벤트</style.CategoryBox>
-          <style.CategoryBox>게임</style.CategoryBox>
-          <style.CategoryBox>인물/블로그</style.CategoryBox>
-          <style.CategoryBox>코미디</style.CategoryBox>
-          <style.CategoryBox>엔터테인먼트</style.CategoryBox>
-          <style.CategoryBox>뉴스/정치</style.CategoryBox>
-          <style.CategoryBox>노하우/스타일</style.CategoryBox>
-          <style.CategoryBox>교육</style.CategoryBox>
-          <style.CategoryBox>과학기술</style.CategoryBox>
-          <style.CategoryBox>비영리/사회운동</style.CategoryBox>
+          {categoryArray.map(category => (
+            <style.CategoryBox key={category.id} value={category.value} onClick={onClick} active={categoryList.find((el) => el === category.value)}>
+              {category.value}
+            </style.CategoryBox>
+          ))}
         </style.CategoryContainer>
         <style.SetBtn type="submit">완료</style.SetBtn>
       </style.SignupBox>
     </style.SignupContainer>
   );
 }
+
+const categoryArray = [
+  {
+    "id": 1,
+    "value": "영화/애니메이션"
+  },
+  {
+    "id": 2,
+    "value": "자동차/교통",
+  },
+  {
+    "id": 3,
+    "value": "음악",
+  },
+  {
+    "id": 4,
+    "value": "애완동물/동물",
+  },
+  {
+    "id": 5,
+    "value": "스포츠",
+  },
+  {
+    "id": 6,
+    "value": "여행/이벤트",
+  },
+  {
+    "id": 7,
+    "value": "게임",
+  },
+  {
+    "id": 8,
+    "value": "인물/블로그",
+  },
+  {
+    "id": 9,
+    "value": "코미디",
+  },
+  {
+    "id": 10,
+    "value": "엔터테인먼트",
+  },
+  {
+    "id": 11,
+    "value": "뉴스/정치",
+  },
+  {
+    "id": 12,
+    "value": "노하우/스타일",
+  },
+  {
+    "id": 13,
+    "value": "교육",
+  },
+  {
+    "id": 14,
+    "value": "과학기술",
+  },
+  {
+    "id": 15,
+    "value": "비영리/사회운동",
+  },
+]
