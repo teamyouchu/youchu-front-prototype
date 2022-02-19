494f8ec3c21bf6d0cff9664b67e98432b50d33ce
Song kyeong seok <skj0922@kookmin.ac.kr>
2022-02-19 12:00:49 +0900

    feat: 모달 닫기 버튼 임시 커밋

--- src/components/Registration.js
+++ src/components/Registration.js
@@ -1,18 +1,18 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import * as style from './styles/RegistrarionStyle';
 
-import { faCircleXmark } from '@fortawesome/free-regular-svg-icons';
 export default function Registration({ registClose }) {
+  const closeRef = useRef();
   const closeModal = ({ target }) => {
     const className = target.getAttribute('class');
     if (className.includes('out-modal__container')) {
-      const close = document.querySelector('#registClose-btn');
-      close.click();
+      closeRef.current.click();
     }
   };
   return (
     <style.ModalContainer className="out-modal__container" onClick={closeModal}>
       <style.Modal width="500px" height="386px" padding="70px 60px">
+        {/* <img src="/images/close-icon.png" alt="close-btn" /> */}
         <style.Span font="SHSN-L" size="25px" margins="0px 0px 30px 0px">
           유튜버 등록
         </style.Span>
@@ -24,9 +24,8 @@ export default function Registration({ registClose }) {
           이해가 안되면 클릭!
         </style.RediretInfo>
         <style.LinkInput placeholder="www.youtube.com"></style.LinkInput>
-        <style.CompleteButton id="registClose-btn" onClick={registClose}>
+        <style.CompleteButton ref={closeRef} onClick={registClose}>
           완료
-          <style.Img icon={faCircleXmark} />
         </style.CompleteButton>
       </style.Modal>
     </style.ModalContainer>
