8dfb2fb311d7e56fdbccdffdf6aede972243fa6e
jisu Seo <jseo9732@gmail.com>
2022-07-05 03:41:22 +0900

    Refactor: 분리 카테고리 컴포넌트

---
+++ src/components/MyCategory.js
@@ -0,0 +1,97 @@
+import * as style from './styles/MyCategoryStyle';
+
+export default function MyCategory({categoryList, setCategoryList}) {  
+    const onClick = (e) => {
+      console.log("target",e.target.value);
+      categoryList.find((el) => {
+        console.log("el",el);
+        console.log(el === e.target.value);
+        return(null);
+      }
+      );
+      const isIncludes = categoryList.find((el) => el === e.target.value);
+      if (isIncludes) {
+        setCategoryList(categoryList.filter((el) => el !== e.target.value));
+      } else {
+        setCategoryList(preList => [...preList, e.target.value]);
+      }
+    };
+  
+    return (
+      <style.CategoryContainer>
+        {categoryArray.map(category => (
+          <style.CategoryBox 
+            key={category.id} 
+            value={category.value} 
+            onClick={onClick} 
+            active={categoryList.find((el) => el === category.value)}
+          >
+            {category.value}
+          </style.CategoryBox>
+        ))}
+      </style.CategoryContainer>
+    );
+}
+
+const categoryArray = [
+    {
+      "id": 1,
+      "value": "영화/애니메이션"
+    },
+    {
+      "id": 2,
+      "value": "자동차/교통",
+    },
+    {
+      "id": 3,
+      "value": "음악",
+    },
+    {
+      "id": 4,
+      "value": "애완동물/동물",
+    },
+    {
+      "id": 5,
+      "value": "스포츠",
+    },
+    {
+      "id": 6,
+      "value": "여행/이벤트",
+    },
+    {
+      "id": 7,
+      "value": "게임",
+    },
+    {
+      "id": 8,
+      "value": "인물/블로그",
+    },
+    {
+      "id": 9,
+      "value": "코미디",
+    },
+    {
+      "id": 10,
+      "value": "엔터테인먼트",
+    },
+    {
+      "id": 11,
+      "value": "뉴스/정치",
+    },
+    {
+      "id": 12,
+      "value": "노하우/스타일",
+    },
+    {
+      "id": 13,
+      "value": "교육",
+    },
+    {
+      "id": 14,
+      "value": "과학기술",
+    },
+    {
+      "id": 15,
+      "value": "비영리/사회운동",
+    },
+  ]
---
+++ src/components/styles/MyCategoryStyle.js
@@ -0,0 +1,29 @@
+import styled from 'styled-components';
+
+export const CategoryContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  margin-bottom: 20px;
+`;
+
+export const CategoryBox = styled.button`
+  height: 30px;
+  background: ${({ active }) => (active ? '#000000' : '#FFFFFF')};
+  border: 1px solid ${({ active }) => (active ? '#000000' : '#DEDEDE')};
+  border-radius: 5px;
+  padding: 8px 10px;
+  margin-right: 10px;
+  margin-bottom: 10px;
+  cursor: pointer;
+
+  text-align: left;
+  font-family: 'SHSN-R';
+  font-size: 12px;
+  line-height: 15px;
+  letter-spacing: 0px;
+  color: ${({ active }) => (active ? '#FFFFFF' : '#94969B')};
+  &:hover {
+    transform: scale(1.1);
+    transition: 0.2s;
+  }
+`;
