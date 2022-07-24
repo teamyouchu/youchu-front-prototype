1b0443eba44a43c0eb3d52583d7e4a140c1b694c
jisu Seo <jseo9732@gmail.com>
2022-07-24 16:35:48 +0900

    Refactor: 개선 코드

--- src/components/App.js
+++ src/components/App.js
@@ -9,45 +9,40 @@ function App() {
   // 로그인 유저 객체 상태값
   const [userObj, setUserObj] = useState(null);
 
-  // 토큰 재요청 함수, 로그인 유저 변경 코드
-  const newToken = async () => {
-    await loginAPI
-      .postRefresh()
-      .then((res) => {
-        localStorage.setItem('refreshToken', res.data.refreshToken);
-        axios.defaults.headers.common[
-          'Authorization'
-        ] = `Bearer ${res.data.accessToken}`;
-        userAPI
-          .getMe()
-          .then(({ data }) => {
-            setUserObj({
-              email: data.email,
-              favoriteCategory: data.favoriteCategory,
-              hasReview: data.hasReview,
-              imageUrl: data.imageUrl,
-              nickname: data.nickname,
-            });
-          })
-          .catch((err) => {
-            console.log(err);
-          });
-      })
-      .catch((err) => {
-        if (err.response.status === 401) {
-          window.localStorage.removeItem('refreshToken');
-          setUserObj(null);
-          alert('인증 정보가 만료되었습니다. 다시 로그인 후 시도해 주세요.');
-        } else {
-          console.log(err);
-        }
-      });
-  };
-
-  // 처음 화면 로드 시 토크 재요청 함수 호출
+  // 처음 화면 로드 시 토큰 재요청, 로그인 유저 변경
   useEffect(() => {
     if (localStorage.getItem('refreshToken')) {
-      newToken();
+      loginAPI
+        .postRefresh()
+        .then((res) => {
+          localStorage.setItem('refreshToken', res.data.refreshToken);
+          axios.defaults.headers.common[
+            'Authorization'
+          ] = `Bearer ${res.data.accessToken}`;
+          userAPI
+            .getMe()
+            .then(({ data }) => {
+              setUserObj({
+                email: data.email,
+                favoriteCategory: data.favoriteCategory,
+                hasReview: data.hasReview,
+                imageUrl: data.imageUrl,
+                nickname: data.nickname,
+              });
+            })
+            .catch((err) => {
+              console.log(err);
+            });
+        })
+        .catch((err) => {
+          if (err.response.status === 401) {
+            window.localStorage.removeItem('refreshToken');
+            setUserObj(null);
+            alert('인증 정보가 만료되었습니다. 다시 로그인 후 시도해 주세요.');
+          } else {
+            console.log(err);
+          }
+        });
     }
   }, []);
 
--- src/components/AppRouter.js
+++ src/components/AppRouter.js
@@ -21,38 +21,33 @@ import {
 export default function AppRouter() {
   const [isShow, setIsShow] = useState(true);
   return (
-    <>
-      <Router>
-        {isShow && <Header />}
-        <div id="wrap">
-          <ScrollToTop>
-            <Switch>
-              <Route exact path="/" component={Home} />
-              <Route path="/search">
-                <Search setIsShow={setIsShow} />
-              </Route>
-              <Route exact path="/youtubers" component={List} />
-              <Route exact path="/youtubers/review/:id" component={Review} />
-              <Route
-                path="/youtubers/review/detail/:id"
-                component={ReviewDetail}
-              />
-              {/* 리뷰하기 버튼 눌렀을 때 id값 넘어가게 수정 */}
-              <Route
-                path="/youtubers/reviewWrite/:id"
-                component={ReviewWrite}
-              />
-              <Route path="/login" component={Login} />
-              <Route path="/signup" component={Signup} />
-              <Route path="/profile" component={Profile} />
-              <Route path="/modifyNickName" component={ModifyNickName} />
-              <Route path="/modifyCategory" component={ModifyCategory} />
-              <Route component={EmptyPage} />
-            </Switch>
-          </ScrollToTop>
-          <div id="footer">{isShow && <Footer />}</div>
-        </div>
-      </Router>
-    </>
+    <Router>
+      {isShow && <Header />}
+      <div id="wrap">
+        <ScrollToTop>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/search">
+              <Search setIsShow={setIsShow} />
+            </Route>
+            <Route exact path="/youtubers" component={List} />
+            <Route exact path="/youtubers/review/:id" component={Review} />
+            <Route
+              path="/youtubers/review/detail/:id"
+              component={ReviewDetail}
+            />
+            {/* 리뷰하기 버튼 눌렀을 때 id값 넘어가게 수정 */}
+            <Route path="/youtubers/reviewWrite/:id" component={ReviewWrite} />
+            <Route path="/login" component={Login} />
+            <Route path="/signup" component={Signup} />
+            <Route path="/profile" component={Profile} />
+            <Route path="/modifyNickName" component={ModifyNickName} />
+            <Route path="/modifyCategory" component={ModifyCategory} />
+            <Route component={EmptyPage} />
+          </Switch>
+        </ScrollToTop>
+        <div id="footer">{isShow && <Footer />}</div>
+      </div>
+    </Router>
   );
 }
