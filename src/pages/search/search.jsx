bd7fbe436e29fa29fbc626831e8b2821cce2f78c
jisu Seo <jseo9732@gmail.com>
2022-08-16 01:28:19 +0900

    Feat: 추가 검색 페이지 연관검색어 컴포넌트

--- src/pages/search/search.jsx
+++ src/pages/search/search.jsx
@@ -1,8 +1,11 @@
-import { useEffect } from 'react';
-import { useHistory } from 'react-router-dom';
 import * as style from './style';
+import { useState, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+import RelatedSearch from 'components/relatedSearch/RelatedSearch';
+// import listAPI from 'lib/api/listAPI';
 
 export default function Search({ setIsShow }) {
+  // 모바일, 태블릿 검색 페이지에 헤더, 푸터 안보이게하는 코드
   useEffect(() => {
     setIsShow(false);
     return () => {
@@ -10,19 +13,71 @@ export default function Search({ setIsShow }) {
     };
   }, [setIsShow]);
 
+  // 등록된 유튜버 검색 api
+  // TODO 서지수 api 완성 시 주석 제거
+  // const [searchResults, setSearchResults] = useState([]);
+  // const getSearchResult = async () => {
+  //   await listAPI
+  //     .getYoutuber(searchValue, 90, 5)
+  //     .then((res) => {
+  //       setSearchResults(res.data.data);
+  //     })
+  //     .catch((err) => console.log(err));
+  // };
+
+  // 검색어
+  const [isSearch, setIsSearch] = useState(false);
+  const [searchValue, setSearchValue] = useState('');
+  const onSearchValueChange = (e) => {
+    setSearchValue(e.target.value);
+    if (e.target.value) {
+      // 검색어가 있으면 연관검색어 표시
+      setIsSearch(true);
+      // getSearchResult(); TODO 서지수 api 완성 시 주석 제거
+    } else {
+      // 검색어가 없으면 연관검색어 미표시
+      setIsSearch(false);
+    }
+  };
+
+  // 검색 submit했을 때, 다음 흐름
   const history = useHistory();
-  const goBack = () => {
-    history.goBack();
+  const onSearch = (e) => {
+    e.preventDefault();
+    history.push({
+      pathname: '/youtubers',
+      state: {
+        searchValue: searchValue,
+      },
+    });
+    setSearchValue('');
+    setIsSearch(false);
   };
 
   return (
     <style.SearchPageContainer>
       <style.HeaderBox>
-        <style.SearchInput placeholder="유튜버 이름으로 검색하세요" />
+        <style.SearchInputBox onSubmit={onSearch}>
+          <style.SearchInput
+            placeholder="유튜버 이름으로 검색하세요"
+            value={searchValue}
+            onChange={onSearchValueChange}
+          />
+          {isSearch && (
+            <RelatedSearch
+              page={'search'}
+              // searchResults={searchResults} TODO 서지수 api 완성 시 주석 제거
+              setSearchValue={setSearchValue}
+              setIsSearch={setIsSearch}
+            />
+          )}
+        </style.SearchInputBox>
         <style.Xmark
           src={require('assets/images/xmark.svg').default}
           alt="empty image"
-          onClick={goBack}
+          onClick={() => {
+            history.goBack();
+          }}
         />
       </style.HeaderBox>
     </style.SearchPageContainer>
