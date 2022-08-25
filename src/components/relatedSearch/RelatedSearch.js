87a528d002bd0257fc7cb7eeb41cf91a71d9dd0c
jisu Seo <jseo9732@gmail.com>
2022-08-26 01:03:09 +0900

    Refactor: 중복 코드 개선

--- src/components/relatedSearch/RelatedSearch.js
+++ src/components/relatedSearch/RelatedSearch.js
@@ -21,59 +21,42 @@ export default function RelatedSearch({
       setKeyIndex(-1);
     };
   }, [setIndex, setKeyIndex]);
+
   return (
     <style.SearchDropdownContainer page={page}>
       {page === 'header' && (
         <style.RelatedSearch>연관 검색어</style.RelatedSearch>
       )}
       <div ref={autoRef}>
-        {!(page === 'registration') &&
-          // TODO 서지수 api 완성 시 Temp 제거
-          searchResults_Temp.map((data, idx) => (
-            <style.SearchResultBox
-              key={data.id}
-              className={index === idx ? 'item_over' : null}
-              page={page}
-              as={Link}
-              to={`/youtubers/review/${data.id}`}
-              onClick={() => {
+        {searchResults.map((data, idx) => (
+          <style.SearchResultBox
+            key={data.id}
+            className={index === idx ? 'item_over' : null}
+            page={page}
+            as={page !== 'registration' ? Link : null}
+            to={`/youtubers/review/${data.id}`}
+            onClick={() => {
+              setIsRelatedSearch(false);
+              if (page !== 'registration') {
                 setSearchValue('');
                 setAutoSearchValue(null);
-                setIsRelatedSearch(false);
-              }}
-              onMouseOver={() => {
-                setIndex(idx);
-              }}
-            >
-              <style.SearchResultImg src={data.thumbnail} alt={data.name} />
-              <style.SearchResultName>{data.title}</style.SearchResultName>
-              <style.SearchResultSubscribers>
-                구독자 {numToKorean(data.subscribe)}명
-              </style.SearchResultSubscribers>
-            </style.SearchResultBox>
-          ))}
-        {page === 'registration' &&
-          searchResults.map((data, idx) => (
-            <style.SearchResultBox
-              key={data.id}
-              className={index === idx ? 'item_over' : null}
-              onClick={() => {
+              } else {
                 setSearchValue(data.title);
                 setAutoSearchValue(data.title);
-                setIsRelatedSearch(false);
                 setChannel({ channel_id: data.id, isExist: data.isExist });
-              }}
-              onMouseOver={() => {
-                setIndex(idx);
-              }}
-            >
-              <style.SearchResultImg src={data.thumbnail} alt={data.name} />
-              <style.SearchResultName>{data.title}</style.SearchResultName>
-              <style.SearchResultSubscribers>
-                구독자 {numToKorean(data.subscribe)}명
-              </style.SearchResultSubscribers>
-            </style.SearchResultBox>
-          ))}
+              }
+            }}
+            onMouseOver={() => {
+              setIndex(idx);
+            }}
+          >
+            <style.SearchResultImg src={data.thumbnail} alt={data.name} />
+            <style.SearchResultName>{data.title}</style.SearchResultName>
+            <style.SearchResultSubscribers>
+              구독자 {numToKorean(data.subscribe)}명
+            </style.SearchResultSubscribers>
+          </style.SearchResultBox>
+        ))}
       </div>
     </style.SearchDropdownContainer>
   );
