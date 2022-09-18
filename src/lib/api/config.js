534a685f0ab4d358f5ebec0d7e9ae0565a118e60
jisu Seo <jseo9732@gmail.com>
2022-09-18 19:17:42 +0900

    Refactor: 토큰 관련 사용자 인증 방식 수정

--- src/lib/api/config.js
+++ src/lib/api/config.js
@@ -7,4 +7,67 @@ axios.defaults.baseURL = apiUrl;
 axios.defaults.xsrfCookieName = 'csrftoken';
 axios.defaults.xsrfHeaderName = 'X-CSRFToken';
 
+const accessToken = localStorage.getItem('accessToken');
+if (accessToken) {
+  axios.defaults.headers.common.Authorization = `Bearer ${accessToken}`;
+}
+
+axios.interceptors.request.use(
+  (config) => {
+    // 요청이 전달되기 전에 작업 수행
+    const accessToken = localStorage.getItem('accessToken');
+    if (accessToken) {
+      axios.defaults.headers.common.Authorization = `Bearer ${accessToken}`;
+    }
+    return config;
+  },
+  (error) => {
+    // 요청 오류가 있는 작업 수행
+    return Promise.reject(error);
+  },
+);
+
+axios.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  async (error) => {
+    const {
+      config,
+      response: {
+        status,
+        data: { type },
+      },
+    } = error;
+    if (status === 401 && accessToken) {
+      console.log(type);
+      if (type === 'expired.refresh_token') {
+        alert('인증 정보가 만료되었습니다. 다시 로그인 후 시도해 주세요.');
+        window.localStorage.removeItem('accessToken');
+        window.localStorage.removeItem('refreshToken');
+      } else {
+        const originalRequest = config;
+        const refreshToken = localStorage.getItem('refreshToken');
+        const { data } = await axios.post(
+          '/refresh',
+          {},
+          {
+            headers: {
+              Authorization: `Bearer ${refreshToken}`,
+            },
+          },
+        );
+        const { accessToken: newAccessToken, refreshToken: newRefreshToken } =
+          data;
+        localStorage.setItem('accessToken', newAccessToken);
+        localStorage.setItem('refreshToken', newRefreshToken);
+        axios.defaults.headers.common.Authorization = `Bearer ${newAccessToken}`;
+        originalRequest.headers.Authorization = `Bearer ${newAccessToken}`;
+        return axios(originalRequest);
+      }
+    }
+    return Promise.reject(error);
+  },
+);
+
 export default axios;
