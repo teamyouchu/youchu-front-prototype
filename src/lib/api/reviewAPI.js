d782cb40422bf1ea51fb5d434a4925aa183a58d7
jisu Seo <jseo9732@gmail.com>
2022-10-03 11:17:18 +0900

    Feat: 베스트 리뷰 조회

--- src/lib/api/reviewAPI.js
+++ src/lib/api/reviewAPI.js
@@ -5,6 +5,10 @@ const reviewAPI = {
   delReview(review_id) {
     return axios.delete(`/review/${review_id}`);
   },
+  // 베스트 리뷰 조회
+  getBestReview(channel_id) {
+    return axios.get(`/review/${channel_id}/bestReview`);
+  },
 };
 
 export default reviewAPI;
