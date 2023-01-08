9514d7a39092e9e40a7e70e4764404f7c0a623cf
jisu Seo <jseo9732@gmail.com>
2023-01-09 01:26:44 +0900

    Style: 안쓰는 코드 삭제

--- src/api/authAPI.js
+++ src/api/authAPI.js
@@ -5,19 +5,6 @@ const authAPI = {
   postLogin(data) {
     return axios.post('/login', data);
   },
-  // 엑세스토큰 만료 시 토큰 재요청
-  postRefresh() {
-    const refreshToken = localStorage.getItem('refreshToken');
-    return axios.post(
-      '/refresh',
-      {},
-      {
-        headers: {
-          Authorization: `Bearer ${refreshToken}`,
-        },
-      },
-    );
-  },
   // 회원가입
   putSignup(data) {
     return axios.put('/register', data);
