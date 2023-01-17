9cdb38ebcc0c24adc18c9b7074addfb2394dc110
jisu Seo <jseo9732@gmail.com>
2023-01-18 00:27:21 +0900

    Refactor: FirstYoutuberList 컴포넌트 분리

---
+++ src/components/firstYoutuberList/FirstYoutuberListStyle.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+export const RecsYoutuberList = styled.div`
+  width: 100%;
+  background: #ffffff 0% 0% no-repeat padding-box;
+  opacity: 1;
+  padding: 24px 24px 30px 24px;
+`;
+
+export const RecsTitle = styled.span`
+  font-family: 'SHSN-B';
+  text-align: left;
+  font-size: 24px;
+  line-height: 35px;
+  letter-spacing: 0px;
+  color: #000000;
+`;
+
+export const RecsTitleBlue = styled(RecsTitle)`
+  color: #5c7fdf;
+`;
