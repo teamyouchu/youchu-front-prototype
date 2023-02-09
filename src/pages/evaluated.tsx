4823275e5756d93cfe75c01158e2cbf91d3f61a9
jisu Seo <jseo9732@gmail.com>
2023-02-09 20:05:34 +0900

    Design: 평가 목록 페이지 추가

---
+++ src/pages/evaluated.tsx
@@ -0,0 +1,26 @@
+import YoutuberList from '@/components/YoutuberList';
+
+export default function Evaluated() {
+  return (
+    <>
+      <div className="evaled_container">
+        <YoutuberList from={'evaled'} />
+      </div>
+
+      <style jsx>{`
+        .evaled_container {
+          width: 400px;
+          margin: 0 auto;
+          background: #f6f7fa 0% 0% no-repeat padding-box;
+          opacity: 1;
+          padding-top: 30px;
+        }
+        @media (max-width: 400px) {
+          .evaled_container {
+            width: 100%;
+          }
+        }
+      `}</style>
+    </>
+  );
+}
