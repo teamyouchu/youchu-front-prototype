52c8df6addb25cfd5b9588584f091cd1d8204f78
jisu Seo <jseo9732@gmail.com>
2023-03-25 06:14:31 +0900

    Docs: aws 배포관련 수정

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  trailingSlash: true,
   reactStrictMode: false,
   images: {
     domains: ['yt3.googleusercontent.com', 'yt3.ggpht.com'],
